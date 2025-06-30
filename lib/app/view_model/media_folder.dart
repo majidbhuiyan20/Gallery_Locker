676b2b77aab8974d28142dd510909e3567337dcc
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-06-30 12:01:06 +0600

    Basic UI and Functionality

---
+++ lib/app/view_model/media_folder.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MediaFolder {
+  final String name;
+  final IconData icon;
+  final String path;
+
+  MediaFolder({
+    required this.name,
+    required this.icon,
+    required this.path,
+  });
+}
